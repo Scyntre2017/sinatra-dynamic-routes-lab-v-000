7167a58a5338a6cad9e3fedc6d38f3d2362e9d6e
Christopher Jones <cjones999983@gmail.com>
2018-07-20 01:03:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@ require "pry"
 class App < Sinatra::Base
 
   get "/reversename/:name" do
-    binding.pry
+    params[:name].reverse
+  end
+
+  get "/square/:number" do
+    num = params[:number].to_i
+    square = num * num
+    square.to_s
   end
 
 end
