41a1b019a80fca0c3688c7fd294b8cba5765f692
Christopher Jones <cjones999983@gmail.com>
2018-07-20 01:31:41 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,6 +19,7 @@ class App < Sinatra::Base
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     string = params.map { |key, value| value }.join(" ")
+    binding.pry
     string.concat(".")
   end
 
